b3732201dfe01dba6d34a1ae3389e86c41fab256
adityadeshmane08 <aadityadeshmane.in@gmail.com>
2025-08-25 21:25:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ document.addEventListener("DOMContentLoaded",function(){
 
 // Dynamic bikes list
 const demoBikes = [
-  {brand:'Honda Activa', location:'Symbiosis', price:'₹99/3hr', img:'assets/b1.jpg', rating:4.8},
-  {brand:'TVS Jupiter', location:'MIT WPU', price:'₹99/3hr', img:'assets/b2.jpg', rating:4.5},
-  {brand:'Yamaha FZ', location:'COEP', price:'₹139/3hr', img:'assets/b3.jpg', rating:4.7}
+  {brand:'Honda Activa', location:'Symbiosis', price:'₹99/3hr', img:'b1.jpg', rating:4.8},
+  {brand:'TVS Jupiter', location:'ADYPSOE', price:'₹99/3hr', img:'b2.jpg', rating:4.5},
+  {brand:'Yamaha FZ', location:'COEP', price:'₹139/3hr', img:'b3.jpg', rating:4.7}
 ];
 const bikesList = document.getElementById('bikes-list');
 if (bikesList) {
